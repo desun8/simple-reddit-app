55ad78de63a874adfbb15f831109358459f289c9
Evgeniy <epchelinov@gmail.com>
2018-07-15 23:11:41 +1000

    add redux persist

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,19 +1,32 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducer from './reducers';
 
 const loggerMiddleware = createLogger();
 
-/* eslint-disable no-underscore-dangle */
-const configureStore = preloadedState => createStore(
-  reducer,
-  preloadedState,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware,
-  ),
-);
-/* eslint-enable */
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['mySubreddits'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const configureStore = (preloadedState) => {
+  const store = createStore(
+    persistedReducer,
+    preloadedState,
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+  const persistor = persistStore(store);
+
+  return { store, persistor };
+};
 
 export default configureStore;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import configureStore from '../configureStore';
 import SearchSubreddit from './SearchSubreddit';
 import Tabs from './Tabs';
 import MainPage from './MainPage';
 
-// import {fetchSearchSubredditIfValid} from '../actions';
-
-
-const store = configureStore();
+const { store, persistor } = configureStore();
 const App = () => (
   <Provider store={store}>
-    <div className="container">
-      <SearchSubreddit />
-      <Tabs />
-      <MainPage />
-    </div>
+    <PersistGate loading={null} persistor={persistor}>
+      <div className="container">
+        <SearchSubreddit />
+        <Tabs />
+        <MainPage />
+      </div>
+    </PersistGate>
   </Provider>
 );
-
-// store.dispatch(fetchSearchSubredditIfValid('reactjs'));
 
 export default App;
